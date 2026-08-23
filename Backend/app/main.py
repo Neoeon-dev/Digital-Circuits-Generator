ee6e4e9a912ebd7fb327e1a8855444bffffc8d0d
Neoeon <neoeon.dev@gmail.com>
2026-08-24 02:11:55 +0530

    V4 modified permissions

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -42,7 +42,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://digital-circuits-generator-4.onrender.com"
+        "https://digital-circuits-generator-4.onrender.com",
+        "https://digital-circuits-generator-pgv0axadf-neoeon-devs-projects.vercel.app/logic-solver"
 
         # Add your deployed Next.js frontend here later.
         # Example:
